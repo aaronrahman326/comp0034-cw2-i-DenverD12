efe67abdecd07d026f1d66015371f1d4efacb3a0
ryanscottgregory326 <nickcknapp@gmail.com>
2023-04-10 20:59:23 +0100

    feat: Updated get countries route for html display

---
+++ tourism_hotels_app/routes_html_display.py
@@ -0,0 +1,35 @@
+from flask import render_template, Blueprint, abort, current_app as app
+from tourism_hotels_app.utilities import get_countries, get_country
+
+
+# Define the Blueprint
+html_display_bp = Blueprint("main", __name__)
+
+
+# @main_bp.errorhandler(500)
+# def internal_server_error(e):
+#     """Custom error message for Internal Server Error status code 500"""
+#     return render_template("500.html"), 500
+
+
+# @main_bp.errorhandler(404)
+# def page_not_found(e):
+#     """Custom error message for Not Found error with status code 404"""
+#     return render_template("404.html"), 404
+
+
+@html_display_bp.route("/")
+def index():
+    """Returns the home page."""
+    response = get_countries()
+    return render_template("index.html", country_list=response)
+
+
+@html_display_bp.route("/display_country/<Country_Name>")
+def display_country(Country_Name):
+    """Returns the country detail page."""
+    current_country = get_country(Country_Name)
+    if current_country:
+        return render_template("country_summary_display.html", country=current_country)
+    else:
+        abort(404)
