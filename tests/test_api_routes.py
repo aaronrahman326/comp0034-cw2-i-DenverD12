829d44a69a8e810b0e6a4a31ec113495ad6bbdf5
ryanscottgregory326 <nickcknapp@gmail.com>
2023-04-10 20:39:45 +0100

    test: Adde parametric, error tests for 1 country

--- tests/test_api_routes.py
+++ tests/test_api_routes.py
@@ -11,60 +11,7 @@ regions_schema = TourismArrivalsSchema(many=True)
 region_schema = TourismArrivalsSchema()
 
 
-@pytest.mark.parametrize(
-    (
-        "country_name",
-        "expected_result_cname",
-        "year_2001",
-        "expected_result_year2001",
-        "Max_number_of_arrivals",
-        "expected_result_max",
-    ),
-    (
-        (
-            "Aruba",
-            "Aruba",
-            "year_2001",
-            1178000,
-            "Max_number_of_arrivals",
-            1951000,
-        ),
-        (
-            "Zimbabwe",
-            "Zimbabwe",
-            "year_2001",
-            2217000,
-            "Max_number_of_arrivals",
-            2580000,
-        ),
-    ),
-)
-def test_when_get_specific_country_then_correct_json_returned(
-    test_client,
-    country_name,
-    expected_result_cname,
-    year_2001,
-    expected_result_year2001,
-    Max_number_of_arrivals,
-    expected_result_max,
-):
-    """
-    GIVEN a running Flask app
-    WHEN the "/noc/<code>" route is requested with the GBR code
-    THEN the response should contain the region UK
-    """
-    response = test_client.get(f"/api/countries/filterby/country/{country_name}")
-    assert response.status_code == 200
-
-    assert isinstance(my_string, str)
-
-    country_json = response.json
-    assert country_json["Country_Name"] == f"{expected_result_cname}"
-    assert country_json["year_2001"] == expected_result_year2001
-    assert country_json["Max_number_of_arrivals"] == expected_result_max
-
-
-def test_get_all_countries(test_client):
+def test_get_all_countries(test_client, expected_keys):
     # Make a GET request to the `/countries` endpoint
     response = test_client.get("/api/countries")
     assert response.status_code == 200
@@ -75,43 +22,93 @@ def test_get_all_countries(test_client):
     # Assert that the response is not empty
     assert len(response_data) > 0
 
+    # Define expected keys from the created fixture
+    expected_keys_dict = expected_keys
+
     # Assert that each item in the response has the expected keys
-    expected_keys = {
-        "Country_Name",
-        "Region",
-        "IncomeGroup",
-        "Country_Code",
-        "Indicator_Name",
-        "year_1995",
-        "year_1996",
-        "year_1997",
-        "year_1998",
-        "year_1999",
-        "year_2000",
-        "year_2001",
-        "year_2002",
-        "year_2003",
-        "year_2004",
-        "year_2005",
-        "year_2006",
-        "year_2007",
-        "year_2008",
-        "year_2009",
-        "year_2010",
-        "year_2011",
-        "year_2012",
-        "year_2013",
-        "year_2014",
-        "year_2015",
-        "year_2016",
-        "year_2017",
-        "year_2018",
-        "year_2019",
-        "year_2020",
-        "Average_10year_in_tourist_arrivals",
-        "Max_number_of_arrivals",
-        "Minimum_number_of_arrivals",
-        "Percent_drop_2019_to_2020",
-    }
     for country_data in response_data:
-        assert set(country_data.keys()) == expected_keys
+        assert set(country_data.keys()) == expected_keys_dict
+
+
+@pytest.mark.parametrize(
+    (
+        "expected_result_cname",
+        "expected_result_year2001",
+        "expected_result_max",
+        "expected_result_ccode",
+    ),
+    (
+        (
+            "Aruba",
+            1178000,
+            1951000,
+            "ABW",
+        ),
+        (
+            "Zimbabwe",
+            2217000,
+            2580000,
+            "ZWE",
+        ),
+        (
+            "India",
+            2537000,
+            17914000,
+            "IND",
+        ),
+    ),
+)
+def test_when_get_specific_country_then_correct_json_returned(
+    test_client,
+    expected_result_cname,
+    expected_result_year2001,
+    expected_result_max,
+    expected_result_ccode,
+):
+    """
+    GIVEN:
+    WHEN:
+    THEN:
+    """
+    response = test_client.get(
+        f"/api/countries/filterby/country/{expected_result_cname}"
+    )
+
+    assert response.status_code == 200
+
+    country_json = response.json
+
+    var_dict = {
+        "Country_Name": expected_result_cname,
+        "year_2001": expected_result_year2001,
+        "Max_number_of_arrivals": expected_result_max,
+        "Country_Code": expected_result_ccode,
+    }
+
+    for key, value in var_dict.items():
+        assert country_json[key] == value
+
+
+@pytest.mark.parametrize("year", ["1995", "2000", "2010", "2015", "2020"])
+def test_by_year_parametrized(test_client, year):
+    # Test case 1: Check if the response is successful and the returned JSON has the correct length
+    response = test_client.get(f'/api/filterby/year/{year}')
+    assert response.status_code == 200
+    assert len(response.json) == 195
+
+    # Test case 2: Check if the returned JSON contains the correct keys
+    response = test_client.get(f'/api/filterby/year/{year}')
+    assert response.status_code == 200
+    assert 'Country_Name' in response.json[0]
+    assert f'year_{year}' in response.json[0]
+
+
+@pytest.mark.parametrize("year", ["1900", "1000", "0", "3000", "text"])
+def test_by_year_invalid_year(test_client, year):
+
+    # Test case 3: Check if the endpoint returns the correct error message for an invalid year
+    response = test_client.get(f'/api/filterby/year/{year}')
+    assert response.status_code == 404
+    assert response.json['status'] == 404
+    assert response.json['error'] == 'Not found'
+    assert response.json['message'] == 'Invalid resource URI: Invalid year'
