cfe4615304cb7b944ca7b0aef01c74684b622e10
ryanscottgregory326 <nickcknapp@gmail.com>
2023-04-12 21:26:59 +0100

    chore: Added meaningful docstrings to config.py

--- tourism_hotels_app/config.py
+++ tourism_hotels_app/config.py
@@ -1,4 +1,4 @@
-"""Flask configuration."""
+"""Flask configuration classes."""
 from pathlib import Path
 
 # Sets the project root folder
@@ -6,8 +6,25 @@ PROJECT_ROOT = Path(__file__).parent
 
 
 class Config:
-    """Base config class with secret key and SQLAlchemy database path."""
+    """
+    Base configuration class for the Flask application.
 
+    This class defines base configuration settings for the Flask app,
+    including the secret key and the path to the SQLAlchemy database.
+    It is the base class that all other configuration classes inherit.
+
+    Attributes:
+        SECRET_KEY (str): A secret key used to sign session cookies
+            and other secure data.
+        SQLALCHEMY_DATABASE_URI (str): The URI path for the SQLite database.
+        SQLALCHEMY_TRACK_MODIFICATIONS (bool): Whether or not to track
+            modifications to database models.
+        SQLALCHEMY_ECHO (bool): Whether or not to show SQL statements for
+            easier debugging.
+        JSON_SORT_KEYS (bool): Whether or not to sort JSON keys.
+    """
+
+    # Define unique secret key and define URI path
     SECRET_KEY = "YY3R4fQ5OmlmVKOSlsVHew"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(
         PROJECT_ROOT.joinpath("data", "tourism_hotels.db")
@@ -19,16 +36,45 @@ class Config:
 
 
 class ProdConfig(Config):
-    """Production config.
+    """
+    Production configuration class for the Flask application.
 
-    Not currently implemented.
+    This class contains configuration settings for when the application is
+    deployed in a production environment. It disables testing, debugging, and
+    tracking features to ensure optimal performance and security.
+
+    Attributes:
+        TESTING (bool): Whether or not to enable testing mode.
+        DEBUG (bool): Whether or not to enable debugging mode.
+        SQLALCHEMY_TRACK_MODIFICATIONS (bool): Whether or not to track
+            modifications to database models.
+        JSON_SORT_KEYS (bool): Whether or not to sort JSON keys.
     """
 
+    # Disable testing, debugging and tracking
+    TESTING = False
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JSON_SORT_KEYS = False
     pass
 
 
 class DevelopmentConfig(Config):
-    """Development config."""
+    """
+    Development configuration class for the Flask application.
+
+    This class defines configuration settings for the Flask app when it
+    is being developed.
+    It enables debugging and testing features, and disables JSON key
+    sorting, all to allow for easier development.
+
+    Attributes:
+        FLASK_ENV (str): The Flask environment, set to 'development'.
+        DEBUG (bool): Whether or not to enable debugging mode.
+        TESTING (bool): Whether or not to enable testing mode.
+        JSON_SORT_KEYS (bool): Whether or not to sort JSON keys.
+    """
 
     FLASK_ENV = "development"
     DEBUG = True
@@ -37,8 +83,24 @@ class DevelopmentConfig(Config):
 
 
 class TestConfig(Config):
-    """Testing config."""
+    """
+    Testing configuration class for the Flask application.
 
+    This class defines configuration settings for the Flask app when
+    it is being tested.
+    It enables testing mode, sets the SQLAlchemy echo mode to True, and
+    disables the CSRF protection for easier testing. 
+    It also sets the server name and disables JSON key sorting
+    for easier testing.
+
+    Attributes:
+        TESTING (bool): Whether or not to enable testing mode.
+        SQLALCHEMY_ECHO (bool): Whether or not to show SQL statements
+        for easier debugging.
+        WTF_CSRF_ENABLED (bool): Whether or not to enable CSRF protection.
+        SERVER_NAME (str): The server name to be used for the application.
+        JSON_SORT_KEYS (bool): Whether or not to sort JSON keys.
+    """
     TESTING = True
     SQLALCHEMY_ECHO = True
     # CSRF set to false to prevent fail during testing
