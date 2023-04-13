ec02456b194206a5bb2477879f0f022eff90f4a5
ryanscottgregory326 <nickcknapp@gmail.com>
2023-04-13 01:23:48 +0100

    fix/chore: fixed all linting errors in get routes

--- tourism_hotels_app/routes_api_obtain_data.py
+++ tourism_hotels_app/routes_api_obtain_data.py
@@ -1,22 +1,19 @@
-# import jwt
-# from functools import wraps
-# from datetime import datetime, timedelta
-# import sys, json
+"""File containing blueprint of routes to get data in various ways."""
 from flask import (
-    current_app as app,
-    request,
     make_response,
     jsonify,
     Blueprint,
 )
+from tourism_hotels_app import db
 
-# from tourism_hotels_app.models import User
-from tourism_hotels_app.utilities import get_country, get_countries, get_year
-from tourism_hotels_app import db, ma
 # Import created Schemas from the schemas.py
 from tourism_hotels_app.schemas import TourismArrivalsSchema
 from tourism_hotels_app.models import TourismArrivals
 from sqlalchemy import desc
+
+# Import helper functions from utilities file,
+# containing error handling and major functions for routes
+from tourism_hotels_app.utilities import get_country, get_countries, get_year
 
 # Blueprint
 obtain_data_api_bp = Blueprint("api_obtain_data", __name__, url_prefix="/api")
@@ -32,29 +29,55 @@ country_schema = TourismArrivalsSchema()
 @obtain_data_api_bp.get("/countries")
 def get_all_countries():
     """
-    Returns a response that contains a list of countries and their
-    international tourism details in JSON.
-    Also returns status code of 200 for OK (success).
+    Endpoint to get the list of countries details.
+
+    All international tourism details returned in JSON format,
+    including data from every column.
+    Args:
+        None
+
+    Returns:
+        response_all_countries: A JSON response object containing a
+        list of countries and their international tourism details
     """
-    # Use helper function from utilities.py to get JSON of all countries data
+    # Use helper function from utilities.py to get JSON of all countries
     all_countries = get_countries()
 
-    response = make_response(all_countries, 200)
-    response.headers["Content-Type"] = "application/json"
-    return response
+    # Create a response object with JSON data and status code of 200
+    response_all_countries = make_response(all_countries, 200)
+    # Set the Content-Type header to application/json
+    response_all_countries.headers["Content-Type"] = "application/json"
+    return response_all_countries
 
 
 @obtain_data_api_bp.get("/countries/country/<country_name>")
 def by_country(country_name):
     """
-    Returns a JSON response with details for a particular country.
+    Return a JSON response with details for a particular country.
+
+    Args:
+        country_name(str): A string entered in the URI representing
+        the name of the country to fetch data for.
+
+    Returns:
+        response: - A JSON response with details for the specified
+                    country, if it exists in database.
+                  - Status code of 200 for OK (success), or 404 for
+                    Not Found (failure).
+
     Also returns status code of 200 for OK (success).
+
     """
+    # Get data for specified country using helper in utilities.py
     country_result = get_country(country_name)
 
+    # If the country exists, return a response with the data,
+    # and a status code of 200
     if country_result:
         response = make_response(country_result, 200)
         response.headers["Content-Type"] = "application/json"
+    # If the country does not exist, return a response with an error
+    # message and a status code of 404
     else:
         message = jsonify(
             {
@@ -71,19 +94,30 @@ def by_country(country_name):
 @obtain_data_api_bp.get("/filterby/year/<chosen_year>")
 def by_year(chosen_year):
     """
-    Returns a JSON response, for a particular year column,
-    returns all countries corresponding to that year column.
-    Also returns status code of 200 for OK (success).
-    """
+    Return a JSON response of all countries in the particular year.
 
+    Args:
+        chosen_year (str): The year value passed in the URI to filter
+        the data by.
+
+    Returns:
+        response: - A JSON response containing all countries and their
+                    details for the given year and a status code of 200
+                    for OK (success).
+                  - Or a error message JSON with status code of 404 for
+                    not found.
+
+    """
     try:
         # Use helper function from utilities.py to get JSON of all
         # countries data
         year_result = get_year(chosen_year)
 
+        # Make response with year JSON data and success status code
         response = make_response(year_result, 200)
         response.headers["Content-Type"] = "application/json"
         return response
+    # If an invalid year is passed, return a 404 error JSON message
     except AttributeError:
         message = jsonify(
             {
@@ -97,50 +131,57 @@ def by_year(chosen_year):
         return response
 
 
-@obtain_data_api_bp.get('/top-10-countries')
+@obtain_data_api_bp.get("/top-10-countries")
 def top_countries():
-    top_10_countries = db.session.query(
-        TourismArrivals.Country_Name,
-        TourismArrivals.Average_10year_in_tourist_arrivals,
-    ).group_by(TourismArrivals.Country_Name).order_by(
-        desc(TourismArrivals.Average_10year_in_tourist_arrivals)
-    ).limit(10).all()
+    """
+    Return JSON of top 10 countries with the highest average arrivals.
 
+    Returns a JSON response containing details of top 10 countries with
+    the highest average tourist arrivals in the last 10 recorded years.
+
+    Args:
+        None
+
+    Returns:
+        response: JSON response containing the names of the top 10
+        countries with the highest average tourist arrivals with the
+        averages.
+
+
+    """
+    # Get top 10 countries based on average tourist arrivals in the
+    # last 10 recorded years
+    top_10_countries = (
+        db.session.query(
+            TourismArrivals.Country_Name,
+            TourismArrivals.Average_10year_in_tourist_arrivals,
+        )
+        .group_by(TourismArrivals.Country_Name)
+        .order_by(desc(TourismArrivals.Average_10year_in_tourist_arrivals))
+        .limit(10)
+        .all()
+    )
+
+    # Create a dictionary with each country and its average data
     result = {"data": []}
     for country in top_10_countries:
-        result["data"].append({"Country_Name": country[0], "Max_number_of_arrivals": country[1]})
+        result["data"].append(
+            {"Country_Name": country[0], "Max_number_of_arrivals": country[1]}
+        )
 
-    response = jsonify(
+    # Create a JSON response message of the data above
+    success_message = jsonify(
         {
-            'Top 10 countries for tourist arrivals': [{
-                'Country_Name': country_name,
-                'Average arrivals in last 10 recorded years': tourist_arrivals
-            } for country_name, tourist_arrivals in top_10_countries]
+            "Top 10 countries for tourist arrivals": [
+                {
+                    "Country_Name": country_name,
+                    "Average arrivals in last 10 recorded years":
+                    tourist_arrivals,
+                }
+                for country_name, tourist_arrivals in top_10_countries
+            ]
         }
     )
+    response = make_response(success_message, 200)
     response.headers["Content-Type"] = "application/json"
-
     return response
-
-
-# @obtain_data_api_bp.get("/top-10-countries")
-# def top_10_countries_2020():
-    # results = (
-    #     db.session.query(
-    #         TourismArrivals.Country_Name,
-    #         TourismArrivals.year_2020
-    #     )
-    #     .order_by(TourismArrivals.year_2020.desc())
-    #     .limit(10)
-    #     .all()
-    # )
-    # top_10_countries = {
-    #     'Top 10 countries for tourist arrivals': [{
-    #         'Country_Name': country_name,
-    #         'Average arrivals in last 10 recorded years': tourist_arrivals
-    #     } for country_name, tourist_arrivals in results]
-    # }
-
-    # response = make_response(top_10_countries, 200)
-    # response.headers["Content-Type"] = "application/json"
-    # return response
