275439a62555ca590ae32fef905d25c781aedef9
ryanscottgregory326 <nickcknapp@gmail.com>
2023-04-13 12:54:27 +0100

    fix: Linting errors fixed and added docstrings

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,20 @@
+"""File that tests the SQLAlchemy database model instances."""
 
 
 def test_create_new_country(country_example_base_valid_model):
     """
-    GIVEN json data for a country
-    WHEN a new country object is created
-    THEN check the fields are defined correctly
+    Tests if the SQLAlchemy model is made correctly.
+
+    Args:
+        country_example_base_valid_model: fixture containing a base
+        country model with all correct features and all columns.
+
+    GIVEN: A fixture with Json data for a country.
+    WHEN: A new country object is created.
+    THEN: Check the fields are defined correctly and match given data.
     """
- 
+
+    # Assert correct fields are defined in model
     assert country_example_base_valid_model.Country_Name == "New Country"
     assert country_example_base_valid_model.Region == "New Region"
     assert country_example_base_valid_model.IncomeGroup == "High Income"
@@ -41,7 +49,8 @@ def test_create_new_country(country_example_base_valid_model):
     assert country_example_base_valid_model.year_2018 == 1
     assert country_example_base_valid_model.year_2019 == 1
     assert country_example_base_valid_model.year_2020 == 1
-    assert country_example_base_valid_model.Average_10year_in_tourist_arrivals == "10.1"
+    assert country_example_base_valid_model.\
+        Average_10year_in_tourist_arrivals == "10.1"
     assert country_example_base_valid_model.Max_number_of_arrivals == 1
     assert country_example_base_valid_model.Minimum_number_of_arrivals == 1
     assert country_example_base_valid_model.Percent_drop_2019_to_2020 == "10%"
